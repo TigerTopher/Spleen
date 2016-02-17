562abf8a5bafcbe99d55711055c6c935608a6bed
Crisostomo Ibarra <christov9999@gmail.com>
2016-02-17 11:39:52 +0800

    Setup for 2nd equation. Not done with the main formula but it is in the documentation

--- quadratic_spline.m
+++ quadratic_spline.m
@@ -38,16 +38,15 @@ function quadratic_spline(data)
 								# First Equation:  Ai-1*(Xi-1)^2 + Bi-1*Xi-1 + Ci-1 = f(Xi-1)
 			temp = [];
 			for k = 1:(3*n-1) 	# Set the default values for the columns for the row in the matrix (a1, a2,..., cn) be zero
-				value = 0;
-				temp = [temp, value];
+				temp = [temp, 0];
 			endfor
-			# Write A
+								# Write A
 			if(j != 2)			# We know that a0 is already zero, so we check that condition.
 				temp(j-2) = (data{i}{j}{1})**2;
 			endif
-			# Write B
+								# Write B
 			temp(n+j-2) = data{i}{j}{1};
-			# Write C
+								# Write C
 			temp(2*n+j-2) = 1;
 			matrix_A = [matrix_A; temp];
 			matrix_B = [matrix_B; data{i}{j}{2}];
@@ -57,29 +56,50 @@ function quadratic_spline(data)
 				value = 0;
 				temp = [temp, value];
 			endfor
-			# Write A
+								# Write A
 			temp(j-1) = (data{i}{j}{1})**2;
-			# Write B
+								# Write B
 			temp(n+j-1) = data{i}{j}{1};
-			# Write C
+								# Write C
 			temp(2*n+j-1) = 1;
 			matrix_A = [matrix_A; temp]
 			matrix_B = [matrix_B; data{i}{j}{2}];
 		endfor
-		matrix_A
-		matrix_B
+
+		# matrix_A				# Un-comment to print the status of Matrix A
+		# matrix_B				# Un-comment to print the status of Matrix B
 								# 3.) The first and last functions must pass through the end points.
+								# Initialize temp that is going to be added to matrix
+		temp = []
+		for k = 1:(3*n-1) 		# Set the default values for the columns for the row in the matrix (a1, a2,..., cn) be zero
+			temp = [temp, 0];
+		endfor
 								# The two equations are:
 								# A1(X0)^(2) + B1X0 + C1 = f(X0)
-								# An(Xn)^(2) + BnXn + Cn = f(Xn)
-								# This gives 2 additional equations. After this, we only need n-1 equations
+								# Since we have set A1 to be zero from earlier, we can remove A1 in the equation.
+								# We have: B1X0 + C1 = f(X0)
+								# Add to Matrix A
+		matrix_A = [matrix_A; temp];
 
+		temp = []
+		for k = 1:(3*n-1) 		# Set the default values for the columns for the row in the matrix (a1, a2,..., cn) be zero
+			temp = [temp, 0];
+		endfor
+
+								# An(Xn)^(2) + BnXn + Cn = f(Xn)
+								# Add to Matrix A
+		matrix_A = [matrix_A; temp];
+		
+								# This gives 2 additional equations. After this, we only need n-1 equations
+								# Add the entry f(X0) to Matrix B
+		# matrix_B = [matrix_B, data{i}{1}{2}];
+								# Add the entry f(Xn) to Matrix B
+		# matrix_B = [matrix_B, data{i}{n+1}{2}];
 
 								# 4.) The first derivative at the interior knots must be equal. The two conditions 
 								# can be represented generally as 2Ai-1*Xi-1 +Bi-1 - 2Ai*Xi - Bi = 0
 								# for i = 2 to n.
 								# This will provide n-1 conditions/equations. We now have all the needed equations.
-
 
 		# for j = 1:length(data{i})-1		# Per pair of entry
 		# endfor
