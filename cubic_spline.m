3d11e5f54b6b93076b85c1a13c6fb65184ab6386
Crisostomo Ibarra <christov9999@gmail.com>
2016-02-19 00:18:28 +0800

    Cubic Spline working

--- cubic_spline.m
+++ cubic_spline.m
@@ -1,7 +1,7 @@
 # Christopher Ivan Vizcarra | 2013-58235 | CS 131 THR
 
 function cubic_spline(data)
-	#figure;
+	figure;
 	for i = 1:length(data)		# Loop the entries that was split by the delimiter 'SHAPE' from earlier
 								# First, get the number of data points and intervals
 								# Note that we have n + 1 data points and
@@ -88,16 +88,104 @@ function cubic_spline(data)
 								# 3. The first derivatives at the interior knots must be equal (n-1 conditions)
 								# f'(x) = 3ax^2 + 2bx + c. So we have
 								# From i = 2:n
-								# 3*Ai-1*(Xi-1)^2 + 2*(Bi-1)*(Xi-1) + (Ci-1) = 3 Ai*(Xi-1)^2 + 2*(Bi)*(Xi-1) + (C)
-								# 3*Ai-1*(Xi-1)^2 + 2*(Bi-1)*(Xi-1) + (Ci-1) - 3 Ai*(Xi-1)^2 - 2*(Bi)*(Xi-1) - (C) = 0
-		
+								# 3*Ai-1*(Xi-1)^2 + 2*(Bi-1)*(Xi-1) + (Ci-1) = 3 Ai*(Xi-1)^2 + 2*(Bi)*(Xi-1) + (Ci)
+								# 3*Ai-1*(Xi-1)^2 + 2*(Bi-1)*(Xi-1) + (Ci-1) - 3 Ai*(Xi-1)^2 - 2*(Bi)*(Xi-1) - (Ci) = 0
+		for j = 2:n
+			temp = [];
+			for k = 1:(4*n)
+				temp = [temp,0];
+			endfor
+								# 3*Ai-1*(Xi-1)^2 + 2*(Bi-1)*(Xi-1) + (Ci-1) - 3 Ai*(Xi-1)^2 - 2*(Bi)*(Xi-1) - (Ci) = 0
 
+								# Write Ai-1
+			temp(j-1) = 3*(data{i}{j}{1})^2;
+								# Write Bi-1
+			temp(n+j-1) = 2*(data{i}{j}{1}); 
+								# Write Ci-1
+			temp(2*n+j-1) = 1; 
+								# Write Ai
+			temp(j) = -3*(data{i}{j}{1})^2;
+								# Write Bi
+			temp(n+j) = -2*(data{i}{j}{1}); 
+								# Write Ci
+			temp(2*n+j) = -1; 
 
-								# 5. 2nd derivatives at the end knots are zero
+			matrix_A = [matrix_A; temp];
+			matrix_B = [matrix_B; 0];
+		endfor
+
+								# 4. The 2nd derivatives at the interior point must be equal (n-1 conditions)
+								# Note f''(x) = 6*Ax + 2B. So we have,
+								# For i = 2:n
+								# 6*Ai-1*Xi-1 + 2Bi-1 = 6Ai*Xi-1 + 2Bi
+								# 6*Ai-1*Xi-1 + 2Bi-1 - 6Ai*Xi-1 - 2Bi = 0
+		for j = 2:n
+			temp = []
+			for k = 1:(4*n)
+				temp = [temp, 0];
+			endfor
+								# Write Ai-1
+			temp(j-1) = 6*data{i}{j}{1};
+								# Write Bi-1
+			temp(n+j-1) = 2; 
+								# Write Ai
+			temp(j) = -6*data{i}{j}{1};
+								# Write Bi
+			temp(n+j) = -2; 
+
+			matrix_A = [matrix_A; temp];
+			matrix_B = [matrix_B; 0];
+		endfor
+
+								# 5. 2nd derivatives at the end knots are zero (2 conditions)
 								# 6*A1X0 + 2B1 = 0
 								# 6*AnXn + 2Bn = 0
+		temp = []
+		for k = 1:(4*n)
+			temp = [temp, 0];
+		endfor
+
+		temp(1) = 6*(data{i}{1}{1});
+		temp(n+1) = 2;
+
+		matrix_A = [matrix_A; temp];
+		matrix_B = [matrix_B; 0];
+
+		temp = []
+		for k = 1:(4*n)
+			temp = [temp, 0];
+		endfor
+
+		temp(n) = 6*(data{i}{n+1}{1});
+		temp(2*n) = 2;
+
+		matrix_A = [matrix_A; temp];
+		matrix_B = [matrix_B; 0];
 	endfor
+
+	solution = matrix_A\matrix_B;
 	matrix_A
 	matrix_B
-	#hold off;
+	solution
+	for j = 1:length(data{i})-1		# Per pair of entry
+		x0 = data{i}{j}{1};
+		y0 = data{i}{j}{2};
+		x1 = data{i}{j+1}{1};
+		y1 = data{i}{j+1}{2};
+		if(x0 > x1)			# We might need to swap the points since we have a range that always go from left to right
+			temp1 = x1;
+			temp2 = y1;
+			x1 = x0;
+			y1 = y0;
+			x0 = temp1;
+			y0 = temp2;
+		endif
+		domain_of_x = x0:0.01:x1;
+
+		our_function = solution(j)*((domain_of_x).**3) + solution(n+j)*((domain_of_x).**2) + solution(2*n+j)*(domain_of_x) + solution(3*n+j);
+		
+		plot(domain_of_x, our_function);
+		hold on;
+	endfor
+	hold off;
 endfunction 
