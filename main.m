ee4bf8d898c865a1eb6aa9aeba1c35b6868afe85
christov9999 <christov9999@gmail.com>
2016-02-19 11:46:31 +0800

    Add documentation on Windows Compatibility

--- main.m
+++ main.m
@@ -1,18 +1,26 @@
 # Christopher Ivan Vizcarra | 2013-58235 | CS 131 THR
 
-#{
+#{	Notes:
 	1. You can run main.m to get all the linear, quadratic, and cubic splines.
 	2. Or you can run each one individually:
 		linear_spline.m
 		quadratic_spline.m
 		cubic_spline.m
 
-	Note: If the variable data has not been passed as a parameter for the spline functions,
+	If the variable data has not been passed as a parameter for the spline functions,
 	(say you called the functions individually and not using main),
 	then the function itself calls the get_the_file function (with the default
 	filename as "input.txt") to get the data itself, then proceed to the spline function.
 
+	[Linux]
 	I am using Octave 3.8.1 and it was configured for "x86_64-pc-linux-gnu".
+
+	[Windows]
+	I was able to run my program in Windows 8.1 using Octave 3.6.4.
+	Since Octave 4.0.0 is unsupported in Windows 8.1 (the one with GUI),
+	I needed to change the graphics backend to 'gnuplot' (which was easily installed in Octave 3.6.4)
+	(although it was around 4 times slower). The Octave CLI was used.
+	Moreover, my terminal says that Octave was configured for "i686-pc-mingw32"
 #}
 
 data = get_the_file("input.txt");
@@ -21,7 +29,5 @@ data = get_the_file("input.txt");
 linear_spline(data);
 quadratic_spline(data);
 cubic_spline(data);
-
-
 
 # Programmed by: Toph Vizcarra, 2016
